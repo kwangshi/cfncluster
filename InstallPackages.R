59db38d94c70af2a8f71728251dd97a07f035dac
kwangshi <kwangshi.shu@gmail.com>
2014-10-28 22:37:28 -0400

    Install R Packages

---
+++ InstallPackages.R
@@ -0,0 +1,57 @@
+       install.packages("Formula", repos="http://cran.us.r-project.org")
+       install.packages("mlogit", repos="http://cran.us.r-project.org"))
+       install.packages("plyr", repos="http://cran.us.r-project.org")
+       install.packages("RColorBrewer", repos="http://cran.us.r-project.org")
+       install.packages("scales", repos="http://cran.us.r-project.org")
+       install.packages("xtable", repos="http://cran.us.r-project.org")
+       install.packages("RUnit", repos="http://cran.us.r-project.org")
+       install.packages("gsDesign", repos="http://cran.us.r-project.org")
+       install.packages("abind", repos="http://cran.us.r-project.org")
+       install.packages("spatstat", repos="http://cran.us.r-project.org")
+       install.packages("bdsmatrix", repos="http://cran.us.r-project.org")
+       
+       install.packages("plm", repos="http://cran.us.r-project.org")
+       install.packages("ggplot2", repos="http://cran.us.r-project.org")
+       install.packages("AER", repos="http://cran.us.r-project.org")
+	install.packages("Hmisc", repos="http://cran.us.r-project.org")
+	install.packages("msm", repos="http://cran.us.r-project.org")
+	install.packages("trust", repos="http://cran.us.r-project.org")
+	install.packages("ergm", repos="http://cran.us.r-project.org")
+	install.packages("coxme", repos="http://cran.us.r-project.org")
+	install.packages("randomForest", repos="http://cran.us.r-project.org")
+	install.packages("mice", repos="http://cran.us.r-project.org")
+	install.packages("timeDate", repos="http://cran.us.r-project.org")
+	install.packages("timeSeries", repos="http://cran.us.r-project.org")
+	install.packages("fBasics", repos="http://cran.us.r-project.org")
+	install.packages("iterators", repos="http://cran.us.r-project.org")
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+	install.packages("doSNOW", repos="http://cran.us.r-project.org")
+	install.packages("latticeExtra", repos="http://cran.us.r-project.org")
+	install.packages("reshape2", repos="http://cran.us.r-project.org")
+	
+#	install.packages("BWidget", repos="http://cran.us.r-project.org")   -- not available for R 3.0.2
+#	install.packages("rpanel", repos="http://cran.us.r-project.org") -- can't find package BWidget
+
+# 	install.packages("rJava", repos="http://cran.us.r-project.org")   -- JDK is missing
+
+#	install.packages("geoR", repos="http://cran.us.r-project.org")
+#	install.packages("geoRglm", repos="http://cran.us.r-project.org")
+	install.packages("R2OpenBUGS", repos="http://cran.us.r-project.org")
+	install.packages("DoseFinding", repos="http://cran.us.r-project.org")
+
+# 	May need to run R CMD javareconf
+#	before install rJavalibrar
+#  
+	install.packages("rJava", repos="http://cran.us.r-project.org")
+	install.packages("ReporteRs", repos="http://cran.us.r-project.org")
+	install.packages("gMCP", repos="http://cran.us.r-project.org")
+#	source("/home/kshu/R/packages/Mediana/mediana_code.txt")
+	source("http://biopharmnet.com/doc/mediana_code.txt")
+	
+	install.packages("doRNG", repos="http://cran.us.r-project.org")
+	install.packages("doParallel", repos="http://cran.us.r-project.org")
+	
+	install.packages("bigmemory", repos="http://cran.us.r-project.org")
+	install.packages("doMC", repos="http://cran.us.r-project.org")
+	install.packages("itertools", repos="http://cran.us.r-project.org")	
+	
